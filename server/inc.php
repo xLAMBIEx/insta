9b759cfb5f868ec780335a180bd359c4a4d2de53
Dominic <mostertcoder@gmail.com>
2020-06-03 10:11:44 +0200

    Update Database Connection Details for dev & live

--- server/inc.php
+++ server/inc.php
@@ -1,9 +1,18 @@
 <?php
   // Database Connection Paramters
-  $DBServ = "localhost";
-  $DBUser = "instaprw_wp244";
-  $DBPass = "CGssfy,!PyW4";
-  $DBName = "instaprw_wp244";
+  $live = false; // Change on live
+
+  if ($live === true) {
+    $DBServ = "localhost";
+    $DBUser = "instaprw_wp244";
+    $DBPass = "CGssfy,!PyW4";
+    $DBName = "instaprw_wp244";
+  } else {
+    $DBServ = "localhost";
+    $DBUser = "root";
+    $DBPass = "";
+    $DBName = "instaplan";
+  }
 
   // Create Database Connection
   $db = new mysqli($DBServ, $DBUser, $DBPass, $DBName);
